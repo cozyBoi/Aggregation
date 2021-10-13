4e1702124f630f530ed67372a01854d52afd59b9
cozyBoi <kjjh1126@sogang.ac.kr>
2021-10-13 16:40:05 +0900

    timer module

--- main.c
+++ main.c
@@ -83,15 +83,10 @@ int main() {
     }
     
     if(pid == 0){
-        struct timespec start, finish, delta;
-        clock_gettime(CLOCK_REALTIME, &start);
-        printf("hi I'm master\n");
         for(i = 0; i < processNum; i++){
             int status;
             wait(&status);
         }
-        printf("master [PID: %d] process finished\n", getpid());
-        fprintf(stdout, "latency : %ld.%.9ld\n", delta.tv_sec, delta.tv_nsec);
     }
     else{
         struct timespec start, finish, delta;
